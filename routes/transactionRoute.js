68149499e8b7b77aed3ff3e8bc980b631b592d66
KameshTripathi <120265582+KameshTripathi@users.noreply.github.com>
2024-04-14 21:31:15 +0530

    Initial commit

---
+++ routes/transactionRoute.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const { addTransaction, getAllTransaction } = require("../controllers/transactionController");
+
+// router object
+const router = express. Router();
+
+// routes
+// add transaction post method
+router.post('/add-transaction', addTransaction)
+// get transaction
+router.get('/get-transaction', getAllTransaction)
+
+module.exports = router;
